df7cb081b0cbe1a40dee9152b051088b4e9c1389
Бонаква <zefirax89@gmail.com>
2025-04-04 17:02:20 +0300

    Add files via upload

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f990563-b3a7-45a2-9c25-70496ef90a99")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using examination_card1; 
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private MainWindow window;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            window = new MainWindow();
+        }
+
+        [TestMethod]
+        public void TestValidScores()
+        {
+            Assert.IsTrue(window.IsValidScore(10, 10));
+            Assert.IsTrue(window.IsValidScore(50, 50));
+            Assert.IsTrue(window.IsValidScore(30, 30));
+            Assert.IsTrue(window.IsValidScore(10, 10));
+        }
+
+        [TestMethod]
+        public void TestInvalidScores()
+        {
+            Assert.IsFalse(window.IsValidScore(-1, 10));
+            Assert.IsFalse(window.IsValidScore(11, 10));
+            Assert.IsFalse(window.IsValidScore(51, 50));
+            Assert.IsFalse(window.IsValidScore(31, 30));
+        }
+
+        [TestMethod]
+        public void TestGradeCalculation()
+        {
+            Assert.AreEqual("5 (отлично)", window.GetGrade(70));
+            Assert.AreEqual("4 (хорошо)", window.GetGrade(40));
+            Assert.AreEqual("3 (удовлетворительно)", window.GetGrade(20));
+            Assert.AreEqual("2 (неудовлетворительно)", window.GetGrade(19));
+        }
+    }
+}
